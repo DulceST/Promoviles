68c408027c84e660ebb03b8fa0ba3fb509eb5c5c
DulceST <silvadulce541@gmail.com>
2024-09-25 19:19:12 -0600

    Se agrego la funcionalidad de camara

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
@@ -14,35 +13,54 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  File? _image;
-  final ImagePicker _picker = ImagePicker();
+  File? _image; //Almacena la imagen seleccionada
+  final ImagePicker _picker = ImagePicker(); // Inicializamos el ImagePicker
 
   // Datos del perfil
   final String name = 'Dulce Silva';
-  final String email = 'Dulce Silva@gmail.com';
+  final String email = 'silvadulce541@gmail.com';
   final String phone = '+1234567890';
   final String github = 'https://github.com/DulceST';
 
   final double profileCompletion = 0.8;
 
+//Inicio seleccion fotos de la galeria
   Future<void> _pickImage() async {
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    final XFile? image = await _picker.pickImage(
+        source: ImageSource.gallery); //abre la galeria y permite seleccionar
+    setState(() {
+      //sirve para actualizar la imagen
+      if (image != null) {
+        //Si se selecciona una imagen, se guarda en la variable
+        _image = File(image.path);
+      }
+    });
+  }
+  //Fin seleccion fotos de la galeria
+
+  //Inicio tomar fotos
+  Future<void> _takePhoto() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
     setState(() {
       if (image != null) {
         _image = File(image.path);
       }
     });
   }
+  //Fin tomar fotos
 
+//Inicio abrir URL
   Future<void> _launchURL(String url) async {
-    final Uri uri = Uri.parse(url); 
+    final Uri uri =
+        Uri.parse(url); //toma la cadena y la cobvierte a un objeto Uri
     if (await canLaunchUrl(uri)) {
-      await launchUrl(uri);
+      //verififca si existe la aplicacion en el dispositivo
+      await launchUrl(uri); //si canLaunchUrl es true abre el URL
     } else {
       print('No se pudo abrir la URL: $url');
     }
   }
-
+  //Fin abrir URL
 
   @override
   Widget build(BuildContext context) {
@@ -65,31 +83,56 @@ class _ProfileScreenState extends State<ProfileScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-            
-            // Imagen de perfil
-            GestureDetector(
-              onTap: _pickImage,
-              child: CircleAvatar(
-                radius: 75,
-                backgroundColor: Colors.grey[300],
-                backgroundImage: _image != null
-                    ? FileImage(_image!)
-                    : const AssetImage('assets/perfil.jpg') as ImageProvider,
-                child: _image == null
-                    ? const Icon(Icons.camera_alt, size: 50, color: Colors.white):null,
-              ),
-            ),
-            //Fin imagen de perfil
+                // Inicio Imagen de perfil
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            ListTile(
+                              leading: const Icon(Icons.camera),
+                              title: const Text('Tomar una foto'),
+                              onTap: () {
+                                _takePhoto(); // Función para tomar una foto
+                                Navigator.pop(context); // Cierra el BottomSheet
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.photo_library),
+                              title: const Text('Seleccionar de la galería'),
+                              onTap: () {
+                                _pickImage(); // Función para seleccionar una imagen de la galería
+                                Navigator.pop(context); // Cierra el BottomSheet
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 75,
+                    backgroundColor: Colors.grey[300],
+                    backgroundImage: _image != null
+                        ? FileImage(_image!)
+                        : const AssetImage('assets/perfil.jpg')
+                            as ImageProvider,
+                  ),
+                ),
+                //Fin imagen de perfil
 
+                const SizedBox(height: 20),
 
-            SizedBox(height: 20),
-
-            // Barra de porcentaje a la derecha
+                // Inicio Barra de porcentaje a la derecha
                 Column(
                   children: [
                     const Text(
                       'Completitud del perfil',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                     const SizedBox(height: 10),
                     CircularPercentIndicator(
@@ -104,14 +147,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ],
             ),
-          
+
             Text(name,
                 style:
                     const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
 
             //Inicio Tarjetas para la informacion del email
             Card(
-              margin: EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(vertical: 10),
               elevation: 5,
               child: ListTile(
                 leading: const Icon(Icons.email, color: Colors.blue),
@@ -125,25 +168,27 @@ class _ProfileScreenState extends State<ProfileScreen> {
             //Fin Tarjetas para la informacion del email
 
             Card(
-              margin: EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(vertical: 10),
               elevation: 5,
               child: ListTile(
-                leading: Icon(Icons.phone, color: Colors.blue),
+                leading: const Icon(Icons.phone, color: Colors.blue),
                 title: GestureDetector(
                   onTap: () => _launchURL('tel:$phone'),
-                  child: Text(phone, style: TextStyle(color: Colors.blue)),
+                  child:
+                      Text(phone, style: const TextStyle(color: Colors.blue)),
                 ),
               ),
             ),
 
             Card(
-              margin: EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(vertical: 10),
               elevation: 5,
               child: ListTile(
-                leading: Icon(Icons.code, color: Colors.blue),
+                leading: const Icon(Icons.code, color: Colors.blue),
                 title: GestureDetector(
                   onTap: () => _launchURL(github),
-                  child: Text(github, style: TextStyle(color: Colors.blue)),
+                  child:
+                      Text(github, style: const TextStyle(color: Colors.blue)),
                 ),
               ),
             ),
