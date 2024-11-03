933bd5c7252eed87ed144d6db4a9250df886a6a9
DulceST <silvadulce541@gmail.com>
2024-11-02 20:22:29 -0600

    Ya se muestran el trailer de las peliculas

--- lib/network/details_api.dart
+++ lib/network/details_api.dart
@@ -4,6 +4,26 @@ import 'package:http/http.dart' as http;
 class DetailsApi {
   final String apiKey = 'a444da131c5cfb31af43b4c296087406';
 
+  // Método para buscar una película por nombre
+  Future<int?> searchMovieByName(String movieName) async {
+    final url = Uri.parse(
+        'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=${Uri.encodeComponent(movieName)}');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['results'].isNotEmpty) {
+        return data['results'][0]['id'];
+      } else {
+        print('No se encontraron películas con ese nombre.');
+        return null;
+      }
+    } else {
+      print('Error al buscar la película: ${response.statusCode}');
+      return null;
+    }
+  }
+
   Future<Map<String, dynamic>?> getMovieTrailer(int movieId) async {
     final url = Uri.parse(
         'https://api.themoviedb.org/3/movie/$movieId/videos?api_key=$apiKey&language=en-US');
@@ -11,10 +31,11 @@ class DetailsApi {
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      return data['results']?.firstWhere((video) => video['type'] == 'Trailer');
+      return data['results']?.firstWhere((video) => video['type'] == 'Trailer', orElse: () => null);
     } else {
-      print('Error fetching trailer: ${response.statusCode}');
+      print('Error al obtener el tráiler: ${response.statusCode}');
       return null;
     }
   }
 }
+
--- lib/screens/details_movie.dart
+++ lib/screens/details_movie.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:pms2024/models/moviesDAO.dart';
+import 'package:pms2024/network/details_api.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class DetailsMovie extends StatefulWidget {
   const DetailsMovie({super.key, required this.moviesDAO});
@@ -12,6 +13,46 @@ class DetailsMovie extends StatefulWidget {
 }
 
 class _DetailsMovieState extends State<DetailsMovie> {
+  final DetailsApi detailsApi = DetailsApi();
+  String? trailerUrl;
+  late YoutubePlayerController _youtubePlayerController;
+
+  Future<void> fetchTrailerUrlByName(String movieName) async {
+    final movieId = await detailsApi.searchMovieByName(movieName);
+    if (movieId != null) {
+      final trailerData = await detailsApi.getMovieTrailer(movieId);
+      if (trailerData != null && trailerData['key'] != null) {
+        setState(() {
+          trailerUrl = trailerData['key'];
+          _youtubePlayerController = YoutubePlayerController(
+            initialVideoId: trailerUrl!,
+            flags: YoutubePlayerFlags(autoPlay: false, mute: false),
+          );
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('No se encontró el tráiler.')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No se encontró la película.')),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTrailerUrlByName(widget.moviesDAO.nameMovie!);
+  }
+
+  @override
+  void dispose() {
+    _youtubePlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +60,11 @@ class _DetailsMovieState extends State<DetailsMovie> {
         children: [
           Positioned.fill(
             child: Image.network(
-              widget.moviesDAO.imgMovie ??'https://i.etsystatic.com/18242346/r/il/933afb/6210006997/il_570xN.6210006997_9fqx.jpg',
+              widget.moviesDAO.imgMovie ?? 
+              'https://i.etsystatic.com/18242346/r/il/933afb/6210006997/il_570xN.6210006997_9fqx.jpg',
               fit: BoxFit.cover,
             ),
           ),
-          // Efecto de difuminado
           Positioned.fill(
             child: Container(
               color: Colors.black.withOpacity(0.4),
@@ -35,7 +76,6 @@ class _DetailsMovieState extends State<DetailsMovie> {
               ),
             ),
           ),
-          // Otros contenidos de la página
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -44,7 +84,13 @@ class _DetailsMovieState extends State<DetailsMovie> {
                   'Detalles de la Película',
                   style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
-                // Otros detalles
+                if (trailerUrl != null)
+                  YoutubePlayer(
+                    controller: _youtubePlayerController,
+                    showVideoProgressIndicator: true,
+                  )
+                else
+                  CircularProgressIndicator(),
               ],
             ),
           ),
